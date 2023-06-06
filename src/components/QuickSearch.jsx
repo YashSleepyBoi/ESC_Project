5f05747ae50772aae5ae6aa9c8101cdbcce2e6b1
Yash Yadav <yash137yadav@gmail.com>
2023-06-06 20:36:19 +0800

    work in proog

---
+++ src/components/QuickSearch.jsx
@@ -0,0 +1,24 @@
+function QuickSearch() {
+    return (
+        // outer container
+        <div className="flex justify-center relative bottom-14" style={{width:'100%'}}>
+            <div class="rounded-full flex align-middle justify-center border-black bg-[#FFE600] h-28" style={{ width: '80%'}}> 
+            {/* inner container using grid */}
+
+                <div class='grid grid-cols-3 gap-2 items-center ' style={{ width: '100%' }}>
+                <div className="flex-column justify-start items-start">
+                        <h2 >Destination</h2>
+                        <h3><i>Choose you destination</i></h3>
+                    </div>
+                    <div>
+                        hi
+                    </div>
+
+            </div>
+        </div>
+        </div>
+        
+    )
+}
+
+export default QuickSearch
