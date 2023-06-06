fc33d38ca60a9dd51878ef59c737a400e2881cf8
Yash Yadav <yash137yadav@gmail.com>
2023-06-06 16:27:22 +0800

    Navbar Complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Navbar from './components/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,41 @@
 function Navbar() {
     
     return (
-        <div className=" grid grid-col-4  gap-4 mt-0 p-auto">
-            <div className="col-span-3 ">
-                <ul className="flex justify-evenly ">
-                    <li>
-                        <a href='https://tailwindcss.com/docs/text-color' className="no-underline text-white ">Home</a>
-                    </li>
-                    <li>
-                        <a href='https://tailwindcss.com/docs/text-color' className="no-underline text-white">Search</a>
-                    </li>
-                    <li>
-                        <a  href='https://tailwindcss.com/docs/text-color'className="no-underline text-white">Attractions</a>
-                    </li>
-                    <li>
-                        <a href='https://tailwindcss.com/docs/text-color' className="no-underline text-white">Rewards</a>
-                        </li>
-                 </ul>
-            </div>
+        <div className=" grid grid-col-4  gap-4 fixed top-0  pt-3 " style={{width:"100%"}}>
             
-            
-            <div className="..." >
-                <ul className=" flex justify-around ">
-                        <li>
-                            <a href='https://tailwindcss.com/docs/text-color' className=" rounded-md border-yellow-400 text-white">Sign Up</a>
-                        </li>
-                        <li>
-                            <a href='https://tailwindcss.com/docs/text-color' className=" rounded-md border-yellow-400 text-white">Login</a>
-                        </li>
-                    </ul>
-            </div>
                 
+            <div class="grid grid-cols-4 gap-4 text-white w-max" style={{width:"100%"}}>
+                <div class="col-span-3 text-white" >
+                    
+                    <ul className="flex justify-evenly ">
+                                        <li>
+                                            <a href='https://tailwindcss.com/docs/text-color' className="no-underline text-white ">Home</a>
+                                        </li>
+                                        <li>
+                                            <a href='https://tailwindcss.com/docs/text-color' className="no-underline text-white">Search</a>
+                                        </li>
+                                        <li>
+                                            <a  href='https://tailwindcss.com/docs/text-color'className="no-underline text-white">Attractions</a>
+                                        </li>
+                                        <li>
+                                            <a href='https://tailwindcss.com/docs/text-color' className="no-underline text-white">Rewards</a>
+                                            </li>
+                    </ul>
+                </div>
+                
+                
+                <div className="text-white" >
+                    <ul className=" flex justify-evenly">
+                            <li>
+                                <a href='https://tailwindcss.com/docs/text-color' className=" rounded-md border-yellow-400 text-white">Sign Up</a>
+                            </li>
+                            <li>
+                                <a href='https://tailwindcss.com/docs/text-color' className=" rounded-md border-yellow-400 text-white">Login</a>
+                            </li>
+                        </ul>
+
+                </div>
+            </div>
                 
             
             
@@ -42,3 +47,6 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+                 
