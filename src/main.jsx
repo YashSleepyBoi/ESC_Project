aff08c2931dd0d059108e5ac2de6cb7883f41b5f
Yash Yadav <yash137yadav@gmail.com>
2023-07-10 15:29:19 +0800

    Room Page-FIX

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Hotel from './Pages/Room_Page/Hotel.jsx'
 import './index.css'
-import Room from './Pages/Room_Page/Room.jsx'
+import Room from './Pages/Room_Page/Hotel.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Room></Room>
+    <Hotel></Hotel>
   </React.StrictMode>,
 )
