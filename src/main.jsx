e5671eb5557369568237be6bccf018c2329035ad
Yash Yadav <yash137yadav@gmail.com>
2023-07-18 12:22:24 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Hotel from './Pages/Room_Page/Hotel.jsx'
 import './index.css'
-import Room from './Pages/Room_Page/Hotel.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
