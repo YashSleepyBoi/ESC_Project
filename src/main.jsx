dda196686c582c1df6fed4369a39cfb825f9e3e2
Yash Yadav <yash137yadav@gmail.com>
2023-07-18 12:27:05 +0800

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Hotel from './Pages/Room_Page/Hotel.jsx'
 import './index.css'
-import Room from './Pages/Room_Page/Room.jsx'
+// import Room from './Pages/Room_Page/Room.jsx'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Room/>
+    <Hotel/>
   </React.StrictMode>,
 )
