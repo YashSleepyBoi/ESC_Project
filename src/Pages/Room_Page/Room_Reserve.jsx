431fcf7dfa930f9fe72d4ae72dec5ac35a775c67
Yash Yadav <yash137yadav@gmail.com>
2023-07-23 19:46:08 +0800

    Revert "Final Changes"
    This reverts commit 67414600f90a6972cc1d86b926fc4c5e464fd4d7.

--- src/Pages/Room_Page/Room_Reserve.jsx
+++ src/Pages/Room_Page/Room_Reserve.jsx
@@ -25,11 +25,6 @@ import { useEffect } from 'react';
 import Banner from './Components/Banner';
 import Panel from "./Components/Panels"
 import 'react-datepicker/dist/react-datepicker.css'
-<<<<<<< Updated upstream
-=======
-
-
->>>>>>> Stashed changes
 
 
 function Room_Reserve() {
@@ -48,21 +43,7 @@ function Room_Reserve() {
 
 
 
-<<<<<<< Updated upstream
      
-=======
-    
-            <div className='room_card_holder'>
-                <CCard className="w-75">
-                    <CCardBody>
-                    <CCardTitle>Card title</CCardTitle>
-                    <CCardText>With supporting text below as a natural lead-in to additional content.</CCardText>
-                    <CButton href="#">Go somewhere</CButton>
-                    </CCardBody>
-                </CCard>
-            </div>
-        
->>>>>>> Stashed changes
         
         
         </>
