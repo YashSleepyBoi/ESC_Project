f5e46532c3abf2aa1e3c9e49b2a22dcd517cfad3
Sarang-Nambiar <101126190+Sarang-Nambiar@users.noreply.github.com>
2023-07-23 19:26:25 +0800

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -2,6 +2,10 @@ import HotelIcon from "@mui/icons-material/Hotel";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import EventAvailableIcon from "@mui/icons-material/EventAvailable";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+
+// Format for input: name of the link to be displayed
+// route for the link which you will be using to redirect to your page
+// icon for the icon to be displayed in the drawer(only in mobile view)
 const links = [
     { name: "Find & Reserve", route: "/findreserve", icon: EventAvailableIcon },
     { name : "Hotels", route: "/hotels", icon: HotelIcon},
