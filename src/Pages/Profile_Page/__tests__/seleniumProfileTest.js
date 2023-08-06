04381eacf781c6626c1ee722a358877fdd325563
TayLuc <tayyclucas@gmail.com>
2023-08-06 20:22:09 +0800

    new file:   src/Pages/Profile_Page/__tests__/seleniumProfileTest.js Added selenium tests for profile page

---
+++ src/Pages/Profile_Page/__tests__/seleniumProfileTest.js
@@ -0,0 +1,72 @@
+import { By, Key, Builder } from "selenium-webdriver";
+import "chromedriver";
+import '@testing-library/jest-dom';
+import useAuth from "../useAuth";
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+describe("selenium Tests", () => {
+
+    var login_target = "http://localhost:1111/login";
+    var register_target = "http://localhost:1111/register"; // Corrected URL for register
+    var profile_target = "http://localhost:1111/profile"; // Corrected URL for profile
+    let driver;
+
+    const password = "password123";
+    const name1 = "seleniumTest";
+    const email1 = "seleniumTest@gmail.com";
+
+    beforeAll(async () => {
+        // Set up the driver before running all
+        driver = await new Builder().forBrowser("chrome").build();
+        jest.setTimeout(30000);
+    });
+
+    afterAll(async () => {
+        //Tear down and delete the test accounts after
+        await driver.quit();
+        const user = useAuth();
+        await db.collection('Users').doc(user.uid).delete();
+        await user.delete();
+    });
+
+    test("should create an account", async () => {
+        await driver.get(register_target);
+        // Create an account called SeleniumTest
+        await driver.findElement(By.id("name")).sendKeys(name1);
+        await driver.findElement(By.id("email")).sendKeys(email1);
+        await driver.findElement(By.id("password")).sendKeys(password);
+        await driver.findElement(By.id("confirmPassword")).sendKeys(password);
+        await driver.findElement(By.className("RegisterBtn")).click();
+        await sleep(2000);
+    });
+
+
+    test("should login to the account", async () => {
+        //Login to the account
+        await driver.get(login_target);
+        await driver.findElement(By.id("email")).sendKeys(email1);
+        await driver.findElement(By.id("password")).sendKeys(password);
+        await driver.findElement(By.className("LoginBtn")).click();
+        await sleep(2000);
+        
+    });
+
+    test("should move to the profile page and check name and email", async () => {
+        // Move to profile page
+        await driver.get(profile_target);
+        
+        await sleep(3000); // Give time for the page to load
+        const nameTest1 = await driver.findElement(By.id("name")).getText();
+        const emailTest1 = await driver.findElement(By.id("email")).getText();
+        console.log(nameTest1);
+        console.log(emailTest1);
+
+        expect(nameTest1).toBe(name1);
+        expect(emailTest1).toBe(email1);
+    });
+
+});
